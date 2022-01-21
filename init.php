67585b8771bbf09ea58264505f52305646e6b3ca
Peter Ade-Ojo <peteradeojo@outlook.com>
2022-01-21 21:26:45 +0100

    help

--- init.php
+++ init.php
@@ -13,6 +13,8 @@ $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 // }
 
+print_r($_ENV);
+
 if (@$_SESSION['login'] !== true) {
   // print_r($_SESSION);
   // echo $_SERVER['SCRIPT_NAME'];
